cda7c59356fb65a5a21d8efe71b0a27e97c9c867
Luciano Bertran <bertranluciano@gmail.com>
2022-11-01 12:05:04 -0300

    go

---
+++ contracts/AssetFactory.sol
@@ -0,0 +1,29 @@
+/// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "./Asset.sol";
+
+contract AssetFactory {
+    Asset[] private _assets;
+
+    function createAsset(
+        uint256 _initialSupply,
+        string memory name_,
+        string memory symbol_
+    ) public {
+        Asset asset = new Asset(
+            _initialSupply,
+            name_,
+            symbol_
+        );
+        _assets.push(asset);
+    }
+
+    function allAssets(uint256 limit, uint256 offset)
+        public
+        view
+        returns (Asset[] memory coll)
+    {
+        return coll;
+    }
+}

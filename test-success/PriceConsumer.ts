565d8c0512146e0f60710457ea58a2d53e3ad4cf
Luciano Bertran <bertranluciano@gmail.com>
2022-12-13 13:00:46 -0300

    test

--- test-success/PriceConsumer.ts
+++ test-success/PriceConsumer.ts
@@ -1,13 +1,8 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { mine } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("PriceConsumer", function () {
-    const ERC20_INITIALSUPLY = 100e10;
-    const LPTOKEN_OTHERACCOUNT = 50e10;
-    const STAKING_OTHERACCOUNT = 30e10;
-    const ONE_WEEK_IN_SECS = 7 * 24 * 60 * 60;
 
     const chainlink_eth = '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419';
     const chainlink_goerli = '0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e';
@@ -17,9 +12,7 @@ describe("PriceConsumer", function () {
         const PriceConsumer = await ethers.getContractFactory("PriceConsumer");
         const priceConsumer = await PriceConsumer.deploy(chainlink_goerli);
         
-        const [owner, otherAccount, secondAccount] = await ethers.getSigners();
-        
-        return { priceConsumer, owner, otherAccount, secondAccount };
+        return { priceConsumer };
     }
 
     describe("Deployment", function () {
